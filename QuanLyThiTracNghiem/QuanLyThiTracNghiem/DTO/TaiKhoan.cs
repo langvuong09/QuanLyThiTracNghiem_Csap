ca86aed0d50d5f3feed72cec31c166f3ed8ccdcd
langvuong09 <cuongcaotien9a@gmail.com>
2025-09-13 13:25:35 +0700

    hello

--- QuanLyThiTracNghiem/QuanLyThiTracNghiem/DTO/TaiKhoan.cs
+++ QuanLyThiTracNghiem/QuanLyThiTracNghiem/DTO/TaiKhoan.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace QuanLyThiTracNghiem.QuanLyThiTracNghiem.DTO
 {
     internal class TaiKhoan
@@ -16,7 +11,6 @@ namespace QuanLyThiTracNghiem.QuanLyThiTracNghiem.DTO
         {
             this.Id = Id;
             this.password = password;
-
         }
     }
 }
